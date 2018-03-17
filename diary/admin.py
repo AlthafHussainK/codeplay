715631b033776de4fa70001d827a0cd3329b9215
arpit <tipra2614@gmail.com>
2018-03-17 13:08:41 +0530

    Initial Commit

---
+++ diary/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import TravelDiary
+
+admin.site.register(TravelDiary)
---
+++ diary/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class TravelDiary(models.Model):
+    date = models.DateTimeField('date published')
+    heading = models.CharField(max_length = 100)
+    body = models.CharField(max_length = 10000)
+
